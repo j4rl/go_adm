4ad1cc67beda6fe375774afcaad08f39b22b9dad
j4rl <charlie.jarl@gmail.com>
2025-04-25 12:40:59 +0200

    db

---
+++ go_adm.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Värd: 127.0.0.1
+-- Tid vid skapande: 25 apr 2025 kl 12:39
+-- Serverversion: 10.4.32-MariaDB
+-- PHP-version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Databas: `go_adm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(40) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `userlevel` int(11) NOT NULL DEFAULT 10,
+  `realname` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumpning av Data i tabell `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `userlevel`, `realname`) VALUES
+(2, 'j4rl', 'e99a18c428cb38d5f260853678922e03', 1000, 'Charlie'),
+(3, 'rolle', '2ba76fc357c80852441116d50eb9bc31', 120, 'Rolle Andersson');
+
+--
+-- Index för dumpade tabeller
+--
+
+--
+-- Index för tabell `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT för dumpade tabeller
+--
+
+--
+-- AUTO_INCREMENT för tabell `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
